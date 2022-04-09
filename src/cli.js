d6e993f7d5f7a8832a60f3b5a4d77b8f77af0e9d
Elena <lekolupaeva1201@gmail.com>
2022-04-09 15:09:11 +0200

    step 3: add cli.js file

---
+++ src/cli.js
@@ -0,0 +1,13 @@
+
+import readlineSync from 'readline-sync';
+
+const getAnswer = (question) => readlineSync.question(question);
+
+const greet = () => {
+    console.log('Welcome to the Brain Games!');
+    const userName = getAnswer('May I have your name? ');
+    console.log(`Hello, ${userName}!`);
+    return userName;
+}
+
+export { getAnswer, greet };
