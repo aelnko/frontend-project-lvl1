96d58eb5aca08f80bd3cd9f32a5aa3e58ac2fabe
Elena <lekolupaeva1201@gmail.com>
2022-04-14 18:30:51 +0200

    fix brain-prime game

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,7 +6,7 @@ const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".
 const getQuestionAndAnswer = () => {
   const number = randomizer();
   const question = `${number}`;
-  for (let i = 2; i <= number/2; i += 1) {
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) {
       const correctAnswer = 'no';
       return [question, correctAnswer];
