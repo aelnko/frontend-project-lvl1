2ed765550c947caf7f5a453d89ff2b512937f580
Elena <lekolupaeva1201@gmail.com>
2022-04-14 23:38:41 +0200

    brain-calc: try to fix error #8953749...

--- src/games/calc.js
+++ src/games/calc.js
@@ -9,15 +9,18 @@ const getQuestionAndAnswer = () => {
   const leftOperand = randomizer();
   const rightOperand = randomizer();
   const operator = operators[randomizer(3)];
-  const question = `${leftOperand} ${operator} ${rightOperand}`;
+  // const question = `${leftOperand} ${operator} ${rightOperand}`;
   if (operator === '+') {
+    const question = `${leftOperand} + ${rightOperand}`;
     const correctAnswer = leftOperand + rightOperand;
     return [question, String(correctAnswer)];
   };
   if (operator === '-') {
+    const question = `${leftOperand} - ${rightOperand}`;
     const correctAnswer = leftOperand - rightOperand;
     return [question, String(correctAnswer)];
   };
+  const question = `${leftOperand} * ${rightOperand}`;
   const correctAnswer = leftOperand * rightOperand;
   return [question, String(correctAnswer)];
 };
