24950cdbd1e4429b4fbf487842c3ad695b077ee7
Elena <lekolupaeva1201@gmail.com>
2022-04-15 16:14:46 +0200

    fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,7 +5,7 @@ const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".
 
 const getQuestionAndAnswer = () => {
   const number = randomizer();
-  const question = `${1}`;
+  const question = `${number}`;
   if (number === 1) {
     const correctAnswer = 'no';
     return [question, correctAnswer];
